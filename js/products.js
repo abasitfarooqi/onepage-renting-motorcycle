5eb4f82cc7cf6c45a05c631f076e233aa64833bb
abasitfarooqi <a.basit.cse@gmail.com>
2024-06-18 17:13:12 +0100

    added more sections and details if needed

--- js/products.js
+++ js/products.js
@@ -31,21 +31,21 @@ function renderProducts(products) {
         const { id, name, image, price, details } = product;
         const { category, top_speed, engine, fuel_capacity, mpg, minimum_hire_period } = details;
 
-        // Create HTML for each product card
+        // Create HTML for each product card with Font Awesome icons
         const productCardHTML = `
         <div class="col-md-4 mb-4">
-            <div class="card" onclick="showProduct('${id}')">
+            <div class="card" onclick="showProduct('${id}')"  data-bs-toggle="modal" data-bs-target="#${id}Modal">
                 <img src="${image}" class="card-img-top" alt="${name}">
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
-                    <p class="card-text">For this or similar model, prices start from ${price} per Week</p>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${id}Modal">More Information</button>
+                    <p class="card-text">For this or similar model, prices start from £${price} per Week</p>
+                    <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#${id}Modal">More Information</button>
                 </div>
             </div>
         </div>
         <!-- ${name} Modal -->
         <div class="modal fade p-0" id="${id}Modal" tabindex="-1" aria-labelledby="${id}ModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-fullscreen ">
+            <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="${id}ModalLabel">${name}</h5>
@@ -58,32 +58,33 @@ function renderProducts(products) {
                                     <img src="${image}" class="img-fluid rounded-start" alt="${name}">
                                 </div>
                                 <div class="col-md-6">
-                                    <p><strong>CATEGORY:</strong> ${category}</p>
-                                    <p><strong>TYPE:</strong> TWIST & GO (AUTOMATIC)</p>
-                                    <ul>
-                                        <li><strong>TOP SPEED:</strong> ${top_speed}</li>
-                                        <li><strong>ENGINE:</strong> ${engine}</li>
-                                        <li><strong>FUEL TANK CAPACITY:</strong> ${fuel_capacity}</li>
-                                        <li><strong>MPG:</strong> ${mpg}</li>
-                                        <li><strong>MINIMUM HIRE PERIOD:</strong> ${minimum_hire_period}</li>
+                                
+                                    <p class="p-0"><strong>Category:</strong> <span class="badge bg-primary">${category}</span> </p>
+                                    <p class="p-0"><strong>Vehicle Type:</strong> <span class="badge bg-info">TWIST & GO (AUTOMATIC)</span></p>
+                                   <ul class="list-unstyled">
+                                        <li><i class="fas fa-tachometer-alt"></i> <strong>Top Speed:</strong> ${top_speed}</li>
+                                        <li><i class="fas fa-cogs"></i> <strong>Engine:</strong> ${engine}</li>
+                                        <li><i class="fas fa-gas-pump"></i> <strong>Fuel Tank Capacity:</strong> ${fuel_capacity}</li>
+                                        <li><i class="fas fa-gas-pump"></i> <strong>MPG:</strong> ${mpg}</li>
+                                        <li><i class="fas fa-calendar-alt"></i> <strong>Minimum Hire Period:</strong> ${minimum_hire_period}</li>
                                     </ul>
-                                    <p><strong>Requirements for Rental:</strong></p>
-                                    <ul>
-                                        <li>Driving Licence</li>
-                                        <li>Proof of Address</li>
-                                        <li>Proof of Identification</li>
-                                        <li>Insurance Certification</li>
-                                        <li>CBT Certification</li>
-                                        <li>£300 deposit + 1 Week Rent</li>
-                                        <li>6 Weeks Minimum Rental Period</li>
-                                        <li>You need to bring a lock and chain before collecting the motorcycle. If you don't have one you can always purchase from our shop along with lot's of other motorcycle accessories.</li>
-                                        <li>Insurance fire and theft is the minimum cover we accept. The motorcycle must be locked at all times.</li>
-                                        <li>Any damage must be paid by you or a claim must be made under your insurance.</li>
-                                        <li>You must give one week notice before returning the motorcycle or deposit will be lost.</li>
-                                        <li>Deposit will be refunded provided there is no damage to the motorcycle and no accessories are missing.</li>
+                                    <h2>Requirements for Rental:</h2>
+                                    <ul class="list-unstyled">
+                                        <li><i class="fas fa-check"></i> Driving Licence</li>
+                                        <li><i class="fas fa-check"></i> Proof of Address</li>
+                                        <li><i class="fas fa-check"></i> Proof of Identification</li>
+                                        <li><i class="fas fa-check"></i> Insurance Certification</li>
+                                        <li><i class="fas fa-check"></i> CBT Certification</li>
+                                        <li><i class="fas fa-check"></i> £300 deposit + 1 Week Rent</li>
+                                        <li><i class="fas fa-check"></i> 6 Weeks Minimum Rental Period</li>
+                                        <li><i class="fas fa-check"></i> You need to bring a lock and chain before collecting the motorcycle. If you don't have one you can always purchase from our shop along with lot's of other motorcycle accessories.</li>
+                                        <li><i class="fas fa-check"></i> Insurance fire and theft is the minimum cover we accept. The motorcycle must be locked at all times.</li>
+                                        <li><i class="fas fa-check"></i> Any damage must be paid by you or a claim must be made under your insurance.</li>
+                                        <li><i class="fas fa-check"></i> You must give one week notice before returning the motorcycle or deposit will be lost.</li>
+                                        <li><i class="fas fa-check"></i> Deposit will be refunded provided there is no damage to the motorcycle and no accessories are missing.</li>
                                     </ul>
                                     <p><strong>£20 RESERVES THIS MOTORCYCLE FOR 48 HOURS</strong></p>
-                                    <p>RESERVE FOR £20 or call 0208 314 1498</p>
+                                    <p class="bg-primary text-white p-2">RESERVE FOR £20 or call 0208 314 1498</p>
                                 </div>
                             </div>
                         </div>
@@ -91,12 +92,13 @@ function renderProducts(products) {
                 </div>
             </div>
         </div>
-        `;
+    `;
 
         // Append product card HTML to products container
         productsContainer.innerHTML += productCardHTML;
     });
 }
+
 
 // Function to initialize
 async function initializeProducts() {
